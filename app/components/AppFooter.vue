8e036bf738c49d7f123dbba8aeb4ff05ecf48c9a
Stephen Cowie <cowiestephenjames@gmail.com>
2017-02-13 08:53:17 -0500

    added top nav and footer components

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,5 +1,9 @@
 <template>
-    <div>Footer</div>
+    <md-bottom-bar class="footer">
+        <md-bottom-bar-item md-icon="history">Recents</md-bottom-bar-item>
+        <md-bottom-bar-item md-icon="favorite" md-active>Favorites</md-bottom-bar-item>
+        <md-bottom-bar-item md-icon="near_me">Nearby</md-bottom-bar-item>
+    </md-bottom-bar>
 </template>
 
 <script>
@@ -15,3 +19,13 @@ export default {
     }
 }
 </script>
+
+<style>
+    .footer {
+        width: 100%;
+        height: 100px; 
+        position: absolute; 
+        bottom: 0px; 
+        left: 0px;
+    }
+</style>
--- app/components/AppNav.vue
+++ app/components/AppNav.vue
@@ -1,6 +1,22 @@
 <template>
     <!-- Fixed navbar -->
-    <div>TopNav</div>
+    <md-toolbar class="md-medium">
+        <div class="md-toolbar-container">
+            <md-button class="md-icon-button">
+                <md-icon>menu</md-icon>
+            </md-button>
+
+            <h2 class="md-title" style="flex: 1;">Medium</h2>
+
+            <md-button class="md-icon-button">
+                <md-icon>search</md-icon>
+            </md-button>
+
+            <md-button class="md-icon-button">
+                <md-icon>filter_list</md-icon>
+            </md-button>
+        </div>
+    </md-toolbar>
 </template>
 
 <script>

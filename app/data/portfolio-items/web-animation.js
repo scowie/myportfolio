b9fd204c8bda06ad539c057cb8cc0388c57d99f8
Stephen Cowie <cowiestephenjames@gmail.com>
2017-03-14 06:32:24 -0400

    begin writeup of animation project

--- app/data/portfolio-items/web-animation.js
+++ app/data/portfolio-items/web-animation.js
@@ -6,7 +6,7 @@ const WebAnimation = {
         'https://s3.amazonaws.com/cowie-public/networkAnimation.mov'
     ],
     timeline: '2015',
-    description: 'This is a test description',
+    description: 'This complex web animation project was built as an assignment for a course in computer networks.  The Greensock Animation Platform (GSAP) was used for the animation.',
     tags: [
         {label: 'GSAP', link: 'https://greensock.com/gsap'},
         {label: 'PHP', link: 'http://php.net/'}

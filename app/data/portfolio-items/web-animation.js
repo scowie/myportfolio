ee118185ee3d8e92ab43f5ab2e49d2f781b5247f
Stephen Cowie <cowiestephenjames@gmail.com>
2017-04-12 04:31:16 -0400

    add notes about web animation proj

--- app/data/portfolio-items/web-animation.js
+++ app/data/portfolio-items/web-animation.js
@@ -7,11 +7,7 @@ const WebAnimation = {
         'https://s3.amazonaws.com/cowie-public/networkAnimation.mov'
     ],
     timeline: '2015',
-    description: 'This complex web animation, built with the <a href="https://greensock.com/gsap" target="_blank">Greensock Animation Platform</a> (GSAP), illustrates how data is transmitted across a computer network.',
-    tags: [
-        // {label: 'GSAP', link: 'https://greensock.com/gsap'},
-        // {label: 'PHP', link: 'http://php.net/'}
-    ]
+    description: 'This complex web animation, built with the <a href="https://greensock.com/gsap" target="_blank">Greensock Animation Platform</a> (GSAP), illustrates how data is transmitted across a computer network.  Digital-to-Analog, Ethernet frame, TCP packet, routing (Dykstra Algorithm), stop-and-wait CRC check'
 }
 
 export default WebAnimation

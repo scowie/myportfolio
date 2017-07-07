6996f0a1990c2854eb15ba0de43b90b2755c1267
Stephen Cowie <cowiestephenjames@gmail.com>
2017-07-07 03:26:28 -0400

    remove unneeded aboutme stuff

--- app/components/AboutMe.vue
+++ app/components/AboutMe.vue
@@ -4,9 +4,7 @@
             <img style="" src="https://s3.amazonaws.com/cowie-public/AAEAAQAAAAAAAAKfAAAAJDNlZGIwM2FkLTgzM2EtNDIzYi04YmM5LTViNDdhNDllOGM3NQ.jpg">
         </div>
 
-            <div id="about-me-writeup-container" style="margin-top:20px; margin-left:2px; min-width:300px; float:none; padding-left:20px; padding-right:20px; vertical-align:top; width:100%;">
-            <p class="about-me-writeup-text" v-html="aboutme.text" style="text-align:justify; font-size:16px; line-height:150%;">
-            </p>
+            
             
         </div>   
          <md-list>
@@ -84,13 +82,11 @@
 </template>
 
 <script>
- import AboutMe from '../data/about-me'
 
 export default {
     name: 'aboutme',
   data () {
     return {
-          aboutme: AboutMe
     }
   },
   methods: {

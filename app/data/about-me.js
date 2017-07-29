2cbe09fb3bbc87c47edd470504d9efd39678cac8
Stephen Cowie <cowiestephenjames@gmail.com>
2017-07-29 12:42:37 -0400

    refine about me writeup to be more concise

--- app/data/about-me.js
+++ app/data/about-me.js
@@ -1,5 +1,5 @@
 const AboutMe = {
-    text: "I am a <b><i>fullstack</i></b> software developer in Greensboro, North Carolina, with a focus on front-end web development.  I have several years of experience building web applications using javascript frameworks/libraries such as <b><i>Angular</i></b>, <b><i>React</i></b>, <b><i>Vue</i></b> and <b><i>JQuery</i></b>.  I enjoy continually learning and trying new technologies, which often make a developer's job faster and easier.  But, it takes more than new tools and technologies to build great software.  I tend to be product-focused, and love the process of bringing an app idea to life, and believe a developer's main goal should be to produce software that users love."
+    text: "I am a <b><i>fullstack</i></b> software developer in Greensboro, North Carolina, with a focus on front-end web development.  I build web applications using javascript frameworks/libraries such as <b><i>Angular</i></b>, <b><i>React</i></b>, <b><i>Vue.js</i></b> and <b><i>JQuery</i></b>.  I am product-focused, and love the process of bringing an app idea to life.  I believe a developer's main goal should be to produce software that users love."
 }
 
 export default AboutMe

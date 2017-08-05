18faf996da18e4c542aa224660083d0ba25d7f0e
Stephen Cowie <cowiestephenjames@gmail.com>
2017-08-05 12:30:33 -0400

    testing bluemix build and deploy

--- app/data/portfolio-items/home-base-x.js
+++ app/data/portfolio-items/home-base-x.js
@@ -8,7 +8,7 @@ const HomeBaseX = {
         'https://s3.amazonaws.com/cowie-public/Screen+Shot+2017-07-30+at+7.33.25+AM.png'
     ],
     timeline: '2015',
-    description: 'HomeX is an app that gives home owners a place to share images and stories of remodelling projects, rate and review products, find local contractors and store important documents.  This app was built using the <b><i>MEAN</i></b> stack (<a href="https://www.mongodb.com/" target="_blank">MongoDB</a>, <a href="http://expressjs.com/" target="_blank">Express</a>, <a href="https://angularjs.org/" target="_blank">Angular</a> and <a href="https://nodejs.org/en/" target="_blank">Node.js</a>).',
+    description: 'HomeX gives home owners a place to share images and stories of remodelling projects, rate and review products, find local contractors and store important documents.  This app was built using the <b><i>MEAN</i></b> stack (<a href="https://www.mongodb.com/" target="_blank">MongoDB</a>, <a href="http://expressjs.com/" target="_blank">Express</a>, <a href="https://angularjs.org/" target="_blank">Angular</a> and <a href="https://nodejs.org/en/" target="_blank">Node.js</a>).',
     tags: [
         // {label: 'AngularJS', link: 'https://angularjs.org/'},
         // {label: 'Bootstrap', link: 'http://getbootstrap.com/'},
